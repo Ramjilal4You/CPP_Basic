e168658c60923047136a4b2c2413416c3e4defe8
Ramjilal Meena <147402456+rm185559@users.noreply.github.com>
2024-03-22 09:08:29 +0530

    20240322

--- Pointers/PointerArithmatics.cpp
+++ Pointers/PointerArithmatics.cpp
@@ -6,6 +6,7 @@ using namespace std;
 // Pointers are incremented and decremented by the size of the data type they point to
 void Increment_Decrement()
 {
+	cout<<"\nInteger Increment & Decrement\n";
 	int a = 22;
 	int* p = &a;
 	printf("p   = %u\n", p);     // p = 223344500  // %p gives an hexa-decimal value, convert it into an unsigned int value by using %u
@@ -14,7 +15,7 @@ void Increment_Decrement()
 
 	printf("--p = %u\n\n", --p); //--p = 223344500	 -4 // restored to original value
 
-
+	cout<<"\nDouble Increment & Decrement\n";
 	double b = 22.22;
 	double* q = &b;
 	printf("q   = %u\n", q);     //q = 223344568
@@ -23,7 +24,7 @@ void Increment_Decrement()
 	
 	printf("--q = %u\n\n", --q); //--q = 223344568	 -8 // restored to original value
 	
-
+	cout<<"\nCharacter Increment & Decrement\n";
 	char c = 'a';
 	char* r = &c;
 	printf("r   = %u\n", r);   //r = 223344628
@@ -38,54 +39,57 @@ void Increment_Decrement()
 // Pointer Addition
 void Pointer_Addition()
 {
+	cout<<"\nAddition of Constant to Pointers\n";
+
 	// Pointer to an integer, Pointer stores the address of N
 	int N = 4;
 	int* ptr1 = &N;
-	cout << "size of int is : " << sizeof(int) << endl;
 
-	cout << "Pointer ptr1 before Addition of 3: ";
+	cout << "\nPointer ptr1(int) before Addition of 3: ";
 	printf("%u \n", ptr1);  // %p gives an hexa-decimal value, convert it into an unsigned int value by using %u
 
 	ptr1 += 3;  //ptr1 = ptr1 + 3*sizeof(int);
-	cout << "Pointer ptr1 after Addition of 3: ";
+	cout << "Pointer ptr1(int) after Addition of 3: ";
 	printf("%u \n\n", ptr1);
 
 
 	//Lets take "pointer to double" example, double size is 8bytes
 	double D = 5.0;
 	double* ptr2 = &D;
-	cout << "size of double is : " << sizeof(double) << endl;
-	cout << "Pointer ptr2 before Addition of 3: ";
+
+	cout << "Pointer ptr2(double) before Addition of 3: ";
 	printf("%u \n", ptr2);
 
 	// Addition of 3 to ptr2
 	ptr2 = ptr2 + 3;	//ptr2 = ptr2 + 3*sizeof(double);
-	cout << "Pointer ptr2 after Addition of 3: ";
+	cout << "Pointer ptr2(double) after Addition of 3: ";
 	printf("%u \n\n", ptr2);
 }
 
 // Subtraction of two pointers
 void Subtraction_of_two_pointers()
 {
+	cout<<"\nSubtraction of Two Pointers of the Same Datatype\n\n";
 	int x = 6; // Integer variable declaration
 	int N = 4;
 
-	int* ptr1 = &x; // stores address of x
-	int* ptr2 = &N; // stores address of N
+	int* ptr1 = &N; // stores address of x
+	int* ptr2 = &x; // stores address of N
 
-	printf(" ptr1 = %u, ptr2 = %u\n", ptr1, ptr2);  // %p gives an hexa-decimal value, convert it into an unsigned int value by using %u
+	printf("ptr1 = %u, ptr2 = %u\n", ptr1, ptr2);  // %p gives an hexa-decimal value, convert it into an unsigned int value by using %u
 
 	int res = ptr2 - ptr1;  // Subtraction of ptr2 - ptr1 //ptr2 - ptr1 = 32, 32/4 = 8
 
-	printf("Subtraction of ptr2 - ptr1 is %d\n\n", res);  // Print x to get the Increment between ptr1 and ptr2
+	printf("Subtraction of ptr2 - ptr1 = %d\n\n", res);  // Print x to get the Increment between ptr1 and ptr2
 }
 
 // Pointer comparision
 void Pointer_comparision()
 {
+	//check if both the pointers are pointing to same element or not
 	int arr[5]; // declaring array
 
-	int* ptr1 = arr;	// declaring pointer to array name
+	int* ptr1 = arr;	// declaring pointer to array name 		// &arr
 
 	int* ptr2 = &arr[0];	// declaring pointer to first element
 
@@ -96,6 +100,7 @@ void Pointer_comparision()
 		cout << "Pointer to Array Name and First Element are not Equal.\n\n";
 	}
 
+	//check if both the pointers are pointing to same location or not
 	int val = 20;
 
 	int* ptr3 = &val;
@@ -107,28 +112,31 @@ void Pointer_comparision()
 	else {
 		cout << "ptr3 and ptr4 are not pointing to same location.\n\n";
 	}
+
+	//to avoid segmentation fault error
+	int *ptr5 = nullptr;
+	if (ptr5 == nullptr) {
+		cout << "ptr5 is not pointing to any Valid location, it is nullptr.\n\n";
+	}
+	else {
+		cout << "ptr5 is pointing to a valid location, It is not nullptr.\n\n";
+	}
 }
 
-void LengthOfArray_PointerArithmatic() {
 
-	int Arr[] = { 1, 2, 3, 4, 5, 6 };
-
-	// variable to store the size of Arr
-	int length = *(&Arr + 1) - Arr;  //Not same as Arr+1
-
-	cout << "Number of elements in Arr[] is: " << length << endl;
-}
 
 int main() {
-	Increment_Decrement();
+	cout << "size of int is : " << sizeof(int) << endl;
+	cout << "size of double is : " << sizeof(double) << endl;
+	cout << "size of char is : " << sizeof(char) << endl<<endl;
 
-	//Pointer_Addition();  //same concept applies for Subtraction  
+	//Increment_Decrement();
+
+	Pointer_Addition();  //same concept applies for Subtraction  
 
 	//Subtraction_of_two_pointers();
 
 	//Pointer_comparision();
-
-	//LengthOfArray_PointerArithmatic();
 
 	return 0;
 }
